576632043344175bd1ef8e892f405f9d5ae01f1a
Gustavo Soares <soaresgu2710@gmail.com>
2024-05-27 20:03:49 -0300

    added svg and icons support with @11ty/eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,38 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets/js/commands.js")
-  eleventyConfig.addPassthroughCopy("src/assets/js/terminal.js")
-  eleventyConfig.setNunjucksEnvironmentOptions({
-    throwOnUndefined: true,
-  });
+const Image = require("@11ty/eleventy-img")
 
-  return {
-    dir: {
-      input: "src",
-    },
-  };
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("src/assets/js/commands.js");
+	eleventyConfig.addPassthroughCopy("src/assets/js/terminal.js");
+	eleventyConfig.setNunjucksEnvironmentOptions({
+		throwOnUndefined: true,
+	});
+
+	// 	let getSvgContent = function (file) {
+	// 		let relativeFilePath = `./src/assets/icons/${file}.svg`;
+	// 		let data = fs.readFileSync(relativeFilePath, (err, contents) => {
+	// 			if (err) return err;
+	// 			return contents;
+	// 		});
+
+	// 		return data.toString("utf8");
+	// 	};
+
+	//   eleventyConfig.addShortcode("svg", getSvgContent)
+	eleventyConfig.addNunjucksAsyncShortcode(
+		"svg",
+		async (file, alt, sizes) => {
+			let metadata = await Image(`src/assets/icons/${file}`, {
+				formats: ["svg"],
+				dryRun: true,
+			});
+
+			return metadata.svg[0].buffer.toString();
+		},
+	);
+
+	return {
+		dir: {
+			input: "src",
+		},
+	};
 };
