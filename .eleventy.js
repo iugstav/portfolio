ba7fb8eb9f6cd37336d860de8d9d8f0dc987d4e3
Gustavo Soares <soaresgu2710@gmail.com>
2024-08-04 12:15:43 -0300

    config

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,17 @@
 const Image = require("@11ty/eleventy-img")
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("src/assets/js/commands.js");
+
+	eleventyConfig.setServerOptions({
+		showAllHosts: true,
+		port: 3001
+	})
+
 	eleventyConfig.addPassthroughCopy("src/assets/js/terminal.js");
+
 	eleventyConfig.setNunjucksEnvironmentOptions({
 		throwOnUndefined: true,
 	});
-
-	// 	let getSvgContent = function (file) {
-	// 		let relativeFilePath = `./src/assets/icons/${file}.svg`;
-	// 		let data = fs.readFileSync(relativeFilePath, (err, contents) => {
-	// 			if (err) return err;
-	// 			return contents;
-	// 		});
-
-	// 		return data.toString("utf8");
-	// 	};
-
-	//   eleventyConfig.addShortcode("svg", getSvgContent)
 	eleventyConfig.addNunjucksAsyncShortcode(
 		"svg",
 		async (file, alt, sizes) => {
