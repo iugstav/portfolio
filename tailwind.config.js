f52df48fdf025b6fa6005bff26b4201990923c81
Gustavo Soares <soaresgu2710@gmail.com>
2024-08-04 12:13:23 -0300

    some tailwindy things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss");
+
 module.exports = {
 	content: ["./src/**/*.njk"],
 	theme: {
+		screens: {
+			"2xl": { max: "1535px" },
+			xl: { max: "1279px" },
+			lg: { max: "1090px" },
+			md: { max: "860px" },
+			sm: { max: "680px" },
+			ph: { max: "539px" },
+		},
 		extend: {
 			colors: {
-				"graphite": "#1a1a1a",
+				graphite: "#1a1a1a",
 				"graphite-hover": "#2c2c2c",
 				"ice-main": "#f1f1f1",
 				"ice-secondary": "#BCBCBC",
@@ -13,10 +24,25 @@ module.exports = {
 					cyan: "#83A598",
 					red: "#fb4934",
 				},
-				"emphasis": "#EBB731",
+				emphasis: "#EBB731",
 			},
 			height: {
 				"first-screen": "calc(100vh - 4rem)",
+				"welcome-normal":
+					"calc(theme(fontSize.8xl)*theme(lineHeight.tight))",
+				"welcome-xl":
+					"calc(theme(fontSize.7xl)*theme(lineHeight.tight))",
+				"welcome-md":
+					"calc(theme(fontSize.9xl)*theme(lineHeight.tight))",
+				"welcome-phone":
+					"calc(theme(fontSize.6xl)*theme(lineHeight.tight))",
+			},
+			gridTemplateColumns: {
+				lang: "1fr 2fr",
+				"lang-small": "2fr 1fr",
+			},
+			gridTemplateRows: {
+				phone: "2fr 1fr",
 			},
 			animation: {
 				"text-slide":
@@ -24,27 +50,39 @@ module.exports = {
 			},
 			keyframes: {
 				"text-slide": {
-					"0%, 16%": {
+					"0%, 20%": {
 						transform: "translateY(0%)",
 					},
-					"20%, 36%": {
-						transform: "translateY(-16.66%)",
+					"25%, 45%": {
+						transform: "translateY(-20%)",
 					},
-					"40%, 56%": {
-						transform: "translateY(-33.33%)",
+					"50%, 70%": {
+						transform: "translateY(-40%)",
 					},
-					"60%, 76%": {
-						transform: "translateY(-50%)",
-					},
-					"80%, 96%": {
-						transform: "translateY(-66.66%)",
+					"75%, 95%": {
+						transform: "translateY(-60%)",
 					},
 					"100%": {
-						transform: "translateY(-83.33%)",
+						transform: "translateY(-80%)",
 					},
 				},
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(
+			function ({ addVariant }) {
+				addVariant("glow", ".glow-capture .glow-overlay &");
+			},
+			{
+				theme: {
+					extend: {
+						colors: {
+							glow: "color-mix(in srgb, red calc(<alpha-value> * 100%), transparent)",
+						},
+					},
+				},
+			},
+		),
+	],
 };
